7ccdf5d7f2c88f3d1da15fa086a70711b2507095
Windy Wang <windy.znw@gmail.com>
2017-02-16 15:00:06 -0500

    Cleaned and wrangled data

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -11,16 +11,108 @@
 #' fetch_data()
 #'
 fetch_data <- function() {
-    # Load the master dataset,
-    ds.prep <- read.table('path/to/file', na = "")
-    # Or ds.prep <- package::package_data
-    # Or ds.prep <- readr::read_csv()
+  # Load the master dataset,
+  ds.prep <- PROMISE::PROMISE %>%
 
-    # Data wrangling commands
+  # Data wrangling commands
+    dplyr::mutate(
+      UDBP = ifelse(UDBP == 0.01, NA, UDBP),
+      UrineCreatinine = ifelse(SID == 2028, 9, UrineCreatinine),
+      ACR = round(UrineMicroalbumin / UrineCreatinine, digits = 2),
+      Ethnicity = as.character(Ethnicity),
+      isAfrican = ifelse(Ethnicity == 'African', 1, 0),
+      Ethnicity = ifelse(
+        Ethnicity %in% c('African', 'First Nations', 'Other'),
+        'Other',
+        Ethnicity
+      ),
+      fVN = factor(
+        VN,
+        levels = c(1, 3, 6),
+        labels = c("Baseline", "3Year", "6Year"),
+        ordered = TRUE
+      ),
+      fMedsBP = factor(
+        MedsBloodPressure,
+        levels = c(0, 1),
+        labels = c("No", "Yes"),
+        ordered = TRUE
+      ),
+      dmStatus = ifelse(DM == 1, "DM",
+                         ifelse(IFG == 1 |
+                                  IGT == 1, "PreDM",
+                                "NGT")),
+      dmStatus = factor(dmStatus,
+                        levels = c("NGT", "PreDM", "DM"),
+                        ordered = TRUE),
+      acrStatus = ifelse(
+        ACR < 2,
+        'Normoalbuminuria',
+        ifelse(ACR > 20, 'Macroalbuminuria',
+               "Microalbuminuria")
+      ),
+      acrStatus = factor(
+        acr_status,
+        levels = c("Normoalbuminuria", "Microalbuminuria", "Macroalbuminuria"),
+        ordered = TRUE
+      ),
+      creat.mgdl = Creatinine * 0.011312,
+      eGFR = nephro::CKDEpi.creat(creat.mgdl, as.numeric(Sex) -
+                                    1, Age, isAfrican),
+      eGFRStatus = cut(eGFR,
+                       breaks = c(-Inf, 60, 90, Inf),
+                       labels = c("Moderate", "Mild", "Normal"),
+                       ordered_result = TRUE),
+      UDBPStatus = cut(UDBP,
+                       breaks = c(0, 1.23, 60, Inf),
+                       labels = c("Trace", "Normal", "High"),
+                       ordered_result = TRUE),
+      udbpCrRatio = UDBP / UrineCreatinine
+    ) %>%
+    dplyr::filter(UDBP < 10000) %>%
+    dplyr::filter(eGFR < 200) %>%
+    dplyr::filter(Creatinine < 200) %>%
+    dplyr::select(
+      SID,
+      BMI,
+      Waist,
+      Age,
+      Sex,
+      Ethnicity,
+      VN,
+      fVN,
+      Glucose0,
+      Glucose120,
+      DM,
+      IFG,
+      IGT,
+      dmStatus,
+      acrStatus,
+      eGFRStatus,
+      UDBPStatus,
+      eGFR,
+      ACR,
+      UrineMicroalbumin,
+      UrineCreatinine,
+      Creatinine,
+      UDBP,
+      udbpCrRatio,
+      VitaminD,
+      MeanArtPressure,
+      Systolic,
+      Diastolic,
+      PTH,
+      ALT,
+      UrinaryCalcium,
+      fMedsBP,
+      SmokeCigs,
+      Canoe,
+      dplyr::matches("meds")
+    )
 
-    # Final dataset object
-    project_data <- ds.prep
+  # Final dataset object
+  project_data <- ds.prep
 
-    # Save the dataset to the data/ folder.
-    devtools::use_data(project_data, overwrite = TRUE)
+  # Save the dataset to the data/ folder.
+  devtools::use_data(project_data, overwrite = TRUE)
 }
