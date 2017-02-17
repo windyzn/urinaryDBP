bfcd91624a50960aebd163c234cf07e6ff14c5c1
Windy Wang <windy.znw@gmail.com>
2017-02-17 16:22:36 -0500

    Allow for pander options for table generation

--- R/setup.R
+++ R/setup.R
@@ -18,11 +18,11 @@ set_options <- function() {
     # Set the options here for individual packages
 
     # For tables (uncomment below if using pander)
-#     pander::panderOptions('table.split.table', Inf)
-#     pander::panderOptions('table.style', 'rmarkdown')
-#     pander::panderOptions('table.alignment.default',
-#                   function(df)
-#                       ifelse(sapply(df, is.numeric), 'center', 'left'))
+    pander::panderOptions('table.split.table', Inf)
+    pander::panderOptions('table.style', 'rmarkdown')
+    pander::panderOptions('table.alignment.default',
+                  function(df)
+                      ifelse(sapply(df, is.numeric), 'center', 'left'))
 
     # For the document (knitr)
     knitr::opts_chunk$set(
