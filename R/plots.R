bf27419ebe1800ecde8f3f642f9707bfb8ec0dd1
Windy Wang <windy.znw@gmail.com>
2017-02-17 17:39:03 -0500

    Moved geom_jitter on top of boxplot

--- R/plots.R
+++ R/plots.R
@@ -15,26 +15,30 @@
 #' @examples
 box_plot <- function(data, xvar, yvar, xlab="", ylab="", facet = FALSE) {
   myboxplot <- ggplot2::ggplot(data, ggplot2::aes_string(x = xvar, y = yvar)) +
-    ggplot2::geom_jitter(ggplot2::aes_string(colour = xvar, alpha = 0.4), width = 0.33) +
     ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar),
                           outlier.shape = NA) +
     ggplot2::stat_summary(geom = "crossbar", width = 0.65, fatten = 0, color = "white",
                           fun.data = function(x){
                             return(c(y = median(x), ymin = median(x), ymax = median(x)))
                           }) +
+    ggplot2::geom_jitter(ggplot2::aes_string(),
+                         position = ggplot2::position_jitter(width = 0.15, height = 0),
+                         alpha = 0.3,
+                         size = 1,
+                         colour = "grey30") +
     ggplot2::theme_minimal() +
     ggplot2::scale_fill_brewer(palette = "Dark2") +
     ggplot2::scale_color_brewer(palette = "Dark2") +
-    ggplot2::scale_fill_brewer() +
     ggplot2::theme(legend.position = "none",
                    panel.grid.major.x = ggplot2::element_blank(),
                    axis.line.y = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_text(colour = "grey"),
                    axis.ticks.y = ggplot2::element_line(colour = "grey"),
-                   axis.text.x = ggplot2::element_text(colour = "grey 30"), #angle = 45
+                   axis.text.x = ggplot2::element_text(colour = "grey30"), #angle = 45
                    axis.title = ggplot2::element_text(size = 10)) +
     ggplot2::xlab(xlab) +
     ggplot2::ylab(ylab)
+
   if(facet == TRUE) {
     myboxplot +
       ggplot2::facet_grid(~fVN, switch = "x")
@@ -71,6 +75,7 @@ scatter_plot = function(data, xvar, yvar, xlab='', ylab='', line = TRUE, facet =
                    axis.ticks.y = ggplot2::element_line(colour = "grey"),
                    axis.text.x = ggplot2::element_text(colour = "grey50"), #angle = 45
                    axis.title = ggplot2::element_text(size = 10)) +
+    ggplot2::scale_fill_brewer(palette = "Dark2") +
     ggplot2::scale_color_brewer(palette = "Dark2") +
     ggplot2::xlab(xlab) +
     ggplot2::ylab(ylab)
