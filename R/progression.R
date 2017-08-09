ce3fe6d34d2ebe7e79bf60be0f8e172e42b590cd
Windy Wang <windy.znw@gmail.com>
2017-08-09 14:55:17 -0400

    combined summary function with plotting function

--- R/progression.R
+++ R/progression.R
@@ -1,18 +1,3 @@
-#' Summarise mean UDBP by a factor variable
-#'
-#' @param data
-#' @param by factor variable to summarise mean UDBP
-#'
-#' @export
-#'
-#' @examples
-#' trend <- summarise_progress_data(ds, by = "fMedsBP")
-summarise_progress_data <- function(data, by) {
-  data %>%
-    dplyr::group_by_(by, "fVN") %>%
-    dplyr::summarise(UDBP=mean(log(UDBP)))
-}
-
 #' Plot progression of UDBP over time (visit number) grouped by variables
 #'
 #' @param data
@@ -25,15 +10,19 @@ summarise_progress_data <- function(data, by) {
 #' @export
 #'
 #' @examples
-plot_progress_data <- function(data, summary_data,
+plot_progress_data <- function(data,
                                xvar = "fVN", yvar = "log(UDBP)",
                                byvar, groupby = "SID") {
+  m <- data %>%
+    dplyr::group_by_(byvar, xvar) %>%
+    dplyr::summarise(UDBP=mean(log(UDBP)))
+
   data %>%
     ggplot2::ggplot(ggplot2::aes_string(x = xvar,
                                y = yvar,
                                colour = byvar)) +
     ggplot2::geom_line(ggplot2::aes_string(group = groupby), alpha = 0.1) +
-    ggplot2::geom_line(data = summary_data, ggplot2::aes_string(group = byvar),
+    ggplot2::geom_line(data = m, ggplot2::aes_string(group = byvar),
                        alpha = 0.7, size = 2) +
     ggplot2::ylab("log(UDBP)")
 }
