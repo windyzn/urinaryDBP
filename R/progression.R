38d36c75fa543d3ea989089a90f6f0660b9245cc
Windy Wang <windy@Windys-MacBook-Pro.local>
2017-08-09 00:20:35 -0400

    initial commit

---
+++ R/progression.R
@@ -0,0 +1,32 @@
+
+summarise_progress_data <- function(data, by) {
+  data %>%
+    dplyr::group_by(by, fVN) %>%
+    dplyr::summarise(UDBP=mean(log(UDBP)))
+}
+
+#' Plot progression of UDBP over time (visit number) grouped by variables
+#'
+#' @param data
+#' @param summary_data
+#' @param xvar
+#' @param yvar
+#' @param byvar
+#' @param groupby
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_progress_data <- function(data, summary_data,
+                               xvar = "fVN", yvar = "log(UDBP)",
+                               byvar, groupby = "SID") {
+  data %>%
+    ggplot2::ggplot(ggplot2::aes_string(x = xvar,
+                               y = yvar,
+                               colour = byvar)) +
+    ggplot2::geom_line(ggplot2::aes_string(group = groupby), alpha = 0.1) +
+    ggplot2::geom_line(data = summary_data, ggplot2::aes_string(group = byvar),
+                       alpha = 0.7, size = 2) +
+    ggplot2::ylab("log(UDBP)")
+}
