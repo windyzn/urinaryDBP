fe86999e4b401a5a1bd94758fb2e4e6f97d7486c
Windy Wang <windy.znw@gmail.com>
2017-08-14 18:21:05 -0400

    initial commit

---
+++ R/regression.R
@@ -0,0 +1,28 @@
+#' Multiple linear regression
+#'
+#' @param data
+#' @param y
+#' @param x
+#' @param covars
+#' @param intvar
+#'
+#' @return
+#'
+#' @examples
+mason_glm <- function(data = project_data,
+                        y = outcomes,
+                        x = predictors,
+                        covars = NULL) {
+
+  co <- !is.null(covars)
+
+  data %>%
+    mason::design("glm") %>%
+    mason:::add_settings(family = stats::gaussian()) %>%
+    mason::add_variables("yvars", y) %>%
+    mason::add_variables("xvars", x) %>%
+    mason::construct() %>%
+
+    mason::construct() %>%
+    mason::scrub()
+}
