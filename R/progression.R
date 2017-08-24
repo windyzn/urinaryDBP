5929c95783ded608d9bb3e007cad26e78cb3cf8a
Windy Wang <windy.znw@gmail.com>
2017-08-24 17:03:02 -0400

    progression plot that does not separate by variable

--- R/progression.R
+++ R/progression.R
@@ -10,12 +10,13 @@
 #' @export
 #'
 #' @examples
-plot_progress_data <- function(data,
+plot_progress_by <- function(data,
                                xvar = "fVN", yvar = "log(UDBP)",
+                               ylab = "log(UDBP)",
                                byvar, groupby = "SID") {
   m <- data %>%
     dplyr::group_by_(byvar, xvar) %>%
-    dplyr::summarise(UDBP=mean(log(UDBP)))
+    dplyr::summarise(UDBP=mean(UDBP))
 
   data %>%
     ggplot2::ggplot(ggplot2::aes_string(x = xvar,
@@ -24,5 +25,24 @@ plot_progress_data <- function(data,
     ggplot2::geom_line(ggplot2::aes_string(group = groupby), alpha = 0.1) +
     ggplot2::geom_line(data = m, ggplot2::aes_string(group = byvar),
                        alpha = 0.7, size = 2) +
-    ggplot2::ylab("log(UDBP)")
+    ggplot2::ylab(ylab)
+}
+
+
+
+
+plot_progress <- function(data = project_data,
+                               xvar = "fVN", yvar,
+                               ylab,
+                               groupby = "SID") {
+  m <- data %>%
+    dplyr::group_by_(yvar, xvar) %>%
+    dplyr::summarise(yvar=mean(yvar))
+
+  data %>%
+    ggplot2::ggplot(ggplot2::aes_string(x = xvar,
+                                        y = yvar)) +
+    ggplot2::geom_line(ggplot2::aes_string(group = groupby), alpha = 0.1) +
+    ggplot2::geom_line(data = m, alpha = 0.7, size = 2) +
+    ggplot2::ylab(ylab)
 }
