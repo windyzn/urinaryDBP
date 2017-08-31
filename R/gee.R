f6af93939766a2c6f25655f170cc3db012ce4dde
Windy Wang <windy.znw@gmail.com>
2017-08-31 16:41:33 -0400

    gee functions specifically for vitamin d analysis

--- R/gee.R
+++ R/gee.R
@@ -3,6 +3,29 @@
 
 # Grab or combine data ----------------------------------------------------
 
+#' Prepare the project data for analysis through GEE. The output is a dataframe
+#' with baseline values.
+#'
+#' @param data
+#'
+#' @export
+prep_mason_data <- function(data) {
+  data %>%
+    dplyr::mutate(
+      UDBP = UDBP/1000, # udbp units to ug/mL
+      udbpBase = ifelse(fVN == "Baseline", UDBP, NA),
+      ageBase = ifelse(fVN == "Baseline", Age, NA),
+      DM = ifelse(DM == 1, "diabetes", "non_dia"),
+      fDM = relevel(as.factor(DM), "non_dia"),
+      Ethnicity = ifelse(Ethnicity == "European", Ethnicity, "Other"),
+      Ethnicity = relevel(as.factor(Ethnicity), "Other")
+    ) %>%
+    dplyr::arrange(SID, fVN) %>%
+    dplyr::group_by(SID) %>%
+    tidyr::fill(udbpBase, ageBase) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(SID, VN)
+}
 
 #' Prepare the project data for analysis through GEE. The output is a dataframe
 #' with baseline values. Subjects with macroalbuminuria and moderate kidney
@@ -10,7 +33,7 @@
 #'
 #' @param data project data
 #' @export
-prep_mason_data <- function(data) {
+prep_mason_data_kidney <- function(data) {
   data %>%
     dplyr::mutate(
       UDBP = UDBP/1000, # udbp units to ug/mL
@@ -124,7 +147,7 @@ explore_gee <- function(data = project_data,
 #' @export
 #'
 #' @examples
-plot_gee_results <- function(results, yvars,
+plot_gee_results_kidney <- function(results, yvars,
                      xlab = "Unit difference with 95% CI in outcome for every unit increase in uVDBP and covariates") {
   results %>%
     dplyr::mutate(Xterms = term) %>%
@@ -140,12 +163,56 @@ plot_gee_results <- function(results, yvars,
                                                  "EthnicityEuropean",
                                                  "BMI",
                                                  "fDMdiabetes")),
-                                  labels = rev(c("uVDBP (ug/mL)",
+                                  labels = rev(c("Baseline uVDBP (ug/mL)",
                                                  "Follow-up Duration (Years)",
                                                  "Baseline Age (Years)",
                                                  "Sex (male)",
                                                  "Ethnicity (European",
                                                  "BMI (kg/m^2)",
+                                                 "Diabetes")),
+                                  ordered = TRUE)) %>%
+    arrange(Xterms) %>%
+    gee_plot(xlab = xlab)
+}
+
+
+
+
+
+#' Plot vitamin D GEE results in a forest plot-style
+#'
+#' @param results
+#' @param yvars
+#' @param xlab
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_gee_results_vitd <- function(results, yvars,
+                                    xlab = "Unit difference with 95% CI in outcome for every unit increase in uVDBP and covariates") {
+  results %>%
+    dplyr::mutate(Xterms = term) %>%
+    dplyr::filter(!term == "(Intercept)") %>%
+    dplyr::mutate(Yterms = factor(Yterms,
+                                  levels = yvars,
+                                  ordered = TRUE),
+                  Xterms = factor(Xterms,
+                                  levels = rev(c("<-Xterm",
+                                                 "VN",
+                                                 "ageBase",
+                                                 "SexMale",
+                                                 "EthnicityEuropean",
+                                                 "BMI",
+                                                 "PTH",
+                                                 "fDMdiabetes")),
+                                  labels = rev(c("Baseline uVDBP (ug/mL)",
+                                                 "Follow-up Duration (Years)",
+                                                 "Baseline Age (Years)",
+                                                 "Sex (male)",
+                                                 "Ethnicity (European",
+                                                 "BMI (kg/m^2)",
+                                                 "Parathyroid Hormone (pmol/L)",
                                                  "Diabetes")),
                                   ordered = TRUE)) %>%
     arrange(Xterms) %>%
