27254d45da8e3aff64f3cb98aedac4bda2b549ca
Windy Wang <windy.znw@gmail.com>
2017-09-18 14:58:44 -0400

    separated gee renaming function from subject characteristic table renaming function

--- R/rename.R
+++ R/rename.R
@@ -1,6 +1,4 @@
 
-# Renaming ----------------------------------------------------------------
-
 #' Renames variables in data set to improve understanding and include units
 #'
 #' @param x
@@ -22,6 +20,7 @@ rename_variables <- function(x) {
     gsub('UrineMicroalbumin', 'Urinary Microalbumin (mg/L)', .) %>%
     gsub('VitaminD', 'Serum 25(OH)D (nmol/L)', .) %>%
     gsub('UDBP', 'Urinary VDBP (ng/mL)', .) %>%
+    gsub("udbpBase", "Baseline uVDBP (ug/mL)", .) %>%
     gsub('udbpCrRatio', 'uVDBP:creatinine (ug/mmol)', .) %>%
     gsub('Diastolic', 'Diastolic Blood Pressure (mmHg)', .) %>%
     gsub('MeanArtPressure', 'Mean Arterial Pressure (mmHg)', .) %>%
@@ -31,8 +30,35 @@ rename_variables <- function(x) {
     gsub('Glucose0', 'Fasting', .) %>%
     gsub('Glucose120', '2h OGTT', .) %>%
     gsub('dm_status', 'Diabetic Status', .) %>%
-    gsub('DM', 'Diabetes', .) %>%
     gsub("fPreDMPreDM", "Prediabetes", .) %>%
+    gsub('DM', 'Diabetes', .) %>%
     gsub('NGT', 'Normal Glucose Tolerance', .) %>%
     gsub("fDysglycemianDysglycemia", "Dysglycemia", .)
+}
+
+
+#' Renames variables in kidney measures GEE results
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rename_gee_kidney <- function(x) {
+  x %>%
+    gsub("<-Xterm", "Baseline uVDBP (ug/mL)", .) %>%
+    gsub("VN", "Follow-up Duration (years)", .) %>%
+    gsub("MonthsFromBaseline", "Follow-up Duration (months)", .) %>%
+    gsub('Age', 'Age (years)', .) %>%
+    gsub("ageBase", "Baseline Age (years)", .) %>%
+    gsub('eGFR', 'Estimated GFR (ml/min/1.73m^2)', .) %>%
+    gsub('ACR', 'Urinary albumin:creatinine (mg/mmol)', .) %>%
+    gsub('UDBP', 'Urinary VDBP (ng/mL)', .) %>%
+    gsub("udbpBase", "Baseline uVDBP (ug/mL)", .) %>%
+    gsub('udbpCrRatio', 'uVDBP:creatinine (ug/mmol)', .) %>%
+    gsub("fPreDMPreDM", "Prediabetes", .) %>%
+    gsub('DM', 'Diabetes', .) %>%
+    gsub('NGT', 'Normal Glucose Tolerance', .) %>%
+    gsub("fDysglycemiaDysglycemia", "Dysglycemia", .)
 }
