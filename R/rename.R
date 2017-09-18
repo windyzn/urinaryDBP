f2ed030654a3238f98cd6562288e85cf181d98d4
Windy Wang <windy.znw@gmail.com>
2017-09-18 14:17:37 -0400

    new variables to be renamed

--- R/rename.R
+++ R/rename.R
@@ -8,16 +8,21 @@
 #' @export
 #'
 #' @examples
-rename_table_rows <- function(x) {
-  gsub('Age', 'Age (years)', x) %>%
+rename_variables <- function(x) {
+  x %>%
+    gsub("VN", "Follow-up Duration (years)", .) %>%
+    gsub("MonthsFromBaseline", "Follow-up Duration (months)", .) %>%
+    gsub('Age', 'Age (years)', .) %>%
+    gsub("ageBase", "Baseline Age (years)", .) %>%
     gsub('Waist', 'Waist Circumference (cm)', .) %>%
     gsub('eGFR', 'Estimated GFR (ml/min/1.73m^2)', .) %>%
-    gsub('ACR', 'Urinary Albumin-to-Creatinine Ratio (mg/mmol)', .) %>%
+    gsub('ACR', 'Urinary albumin:creatinine (mg/mmol)', .) %>%
     gsub('MicroalbCreatRatio', 'Microalbumin:Creatinine', .) %>%
     gsub('UrineCreatinine', 'Urinary Creatinine (mmol/L)', .) %>%
     gsub('UrineMicroalbumin', 'Urinary Microalbumin (mg/L)', .) %>%
     gsub('VitaminD', 'Serum 25(OH)D (nmol/L)', .) %>%
     gsub('UDBP', 'Urinary VDBP (ng/mL)', .) %>%
+    gsub('udbpCrRatio', 'uVDBP:creatinine (ug/mmol)', .) %>%
     gsub('Diastolic', 'Diastolic Blood Pressure (mmHg)', .) %>%
     gsub('MeanArtPressure', 'Mean Arterial Pressure (mmHg)', .) %>%
     gsub('Systolic', 'Systolic Blood Pressure (mmHg)', .) %>%
@@ -27,5 +32,7 @@ rename_table_rows <- function(x) {
     gsub('Glucose120', '2h OGTT', .) %>%
     gsub('dm_status', 'Diabetic Status', .) %>%
     gsub('DM', 'Diabetes', .) %>%
-    gsub('NGT', 'Normal', .)
+    gsub("fPreDMPreDM", "Prediabetes", .) %>%
+    gsub('NGT', 'Normal Glucose Tolerance', .) %>%
+    gsub("fDysglycemianDysglycemia", "Dysglycemia", .)
 }
