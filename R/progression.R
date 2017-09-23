e686994c47d38e0b7b9b3aa486f9da5b3396d303
Windy Wang <windy@Windys-MacBook-Pro.local>
2017-09-23 13:58:55 -0400

    plot_progress_boxplot: plot progression using boxplot

--- R/progression.R
+++ R/progression.R
@@ -55,3 +55,36 @@ plot_progress <- function(data = project_data,
                        stat = "summary", fun.y = mean) +
     ggplot2::ylab(ylab)
 }
+
+
+# Boxplot -----------------------------------------------------------------
+
+plot_progress_boxplot <- function (data,
+                                   xvar = "fVN",
+                                   yvar, ylab,
+                                   groupby = "SID") {
+
+  data %>%
+  ggplot2::ggplot(ggplot2::aes_string(x = xvar,
+                                      y = yvar)) +
+  ggplot2::geom_line(data = data, ggplot2::aes_string(group = groupby), alpha = 0.7,
+                     colour = "#cccccc") +
+
+  ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar)) +
+  ggplot2::stat_summary(geom = "crossbar", width = 0.65, fatten = 0, color = "white",
+                        fun.data = function(x){
+                          return(c(y = median(x), ymin = median(x), ymax = median(x)))
+                        }) +
+
+  ggplot2::theme_minimal() +
+  ggplot2::scale_fill_brewer(palette = "Dark2") +
+  ggplot2::scale_color_brewer(palette = "Dark2") +
+  ggplot2::theme(legend.position = "none",
+                  panel.grid.major.x = ggplot2::element_blank(),
+                  axis.line.y = ggplot2::element_blank(),
+                  axis.text.y = ggplot2::element_text(colour = "grey"),
+                  axis.ticks.y = ggplot2::element_line(colour = "grey"),
+                  axis.text.x = ggplot2::element_text(colour = "grey30"), #angle = 45
+                  axis.title = ggplot2::element_text(size = 10)) +
+  ggplot2::ylab(ylab)
+}
